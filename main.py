40fcd945ce557deb7d557e9595b92c31d60dd9ce
emaneric <eric.n.tech@gmail.com>
2023-07-09 19:44:00 +0100

    na

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from smbus2 import SMBus
+import time
 
 # Open i2c bus 1 and read one byte from address 80, offset 0
 bus = SMBus(1)
-b = bus.read_byte_data(0x68, 0)
-print(b)
+
+while (True)
+    b = bus.read_byte_data(0x68, 0)
+    print(b)
+    time.sleep(1);
+
 bus.close()
