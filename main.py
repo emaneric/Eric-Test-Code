481f087b4c633a1b9130cffd5ee3d13073d6ee96
emaneric <eric.n.tech@gmail.com>
2023-07-09 19:45:21 +0100

    n

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 # Open i2c bus 1 and read one byte from address 80, offset 0
 bus = SMBus(1)
 
-while (True)
+while (True):
     b = bus.read_byte_data(0x68, 0)
     print(b)
     time.sleep(1);
