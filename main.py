5cde82789abcb98a0ac61c007194868f5f990a84
emaneric <eric.n.tech@gmail.com>
2023-07-09 19:14:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from smbus2 import SMBus, i2c_msg
 
 with SMBus(1) as bus:
-    # Read 64 bytes from address 80
-    msg = i2c_msg.read(68, 1)
-    bus.i2c_rdwr(msg)
+    # Read 1 bytes from address 68
+    #msg = i2c_msg.read(68, 1)
+    #bus.i2c_rdwr(msg)
+    print(read_byte(68rce=None))
